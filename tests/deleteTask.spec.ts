86509e715a80a1bb8b47e63861a0a79883492df7
Akshays1405 <akshay.s@revenuehero.io>
2025-05-13 15:42:24 +0530

    removed unused waits

--- tests/deleteTask.spec.ts
+++ tests/deleteTask.spec.ts
@@ -4,12 +4,8 @@ import { Helper } from "./pageObjects/utils/helper";
 
 test('Delete a task from the first column', async ({ page }) => {
   const kanban = new KanbanMainPage(page);
-  const helper = new Helper(page);
 
   await page.goto("https://kanban-566d8.firebaseapp.com/");
-  
-  await helper.sleep(3);
-  
   
   await kanban.deleteTaskAndVerify();
   
